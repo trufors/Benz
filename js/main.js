8d2ed088bf3cd020c09f4c572199da94d2508b1e
trufors <yungbatya@icloud.com>
2022-04-05 22:50:41 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth:true,
+        prevArrow: '<img class="arrow arrow-left" src="images/arrowleft.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="images/arrowright.svg" alt="">',
+    });
 });
